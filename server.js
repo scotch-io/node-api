4e9f27ea01a84343fa9437438314920de1c156de
Guido Barosio <gbarosio@encriptame.com>
2017-10-02 17:29:40 -0300

    added database connection exception into console.log(). Makes debugging easier IMHO

--- server.js
+++ server.js
@@ -16,8 +16,19 @@ app.use(bodyParser.json());
 
 var port     = process.env.PORT || 8080; // set our port
 
+// DATABASE SETUP
 var mongoose   = require('mongoose');
 mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o'); // connect to our database
+
+// Handle the connection event
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function() {
+  console.log("DB connection alive");
+});
+
+// Bear models lives here
 var Bear     = require('./app/models/bear');
 
 // ROUTES FOR OUR API
